3a1581270c3271ed91b8dd48650cf5e1e0226234
AIMAD KACEM <kacem.aimad@gmail.com>
2024-05-26 09:59:28 +0100

    add count to api

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,6 +3,12 @@
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
@@ -12,4 +18,9 @@ def get_status():
 @app_views.route('/stats', methods=['GET'])
 def stats_count():
     """return number of each object"""
-    return storage.count()
+    classes = {"amenities": Amenity, "cities": City,"places": Place,
+           "reviews": Review, "states": State, "users": User}
+    counts ={}
+    for key, obj in classes.items():
+        counts[key] = storage.count(obj)
+    return jsonify(counts)
--- test.py
+++ test.py
@@ -4,11 +4,15 @@
 from models import storage
 from models.state import State
 from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-
-print("All objects: {}".format(storage.count()))
-print("State objects: {}".format(storage.count(State)))
-print("City objects: {}".format(storage.count(City)))
-first_state_id = list(storage.all(State).values())[0].id
-print("First state: {}".format(storage.get(State, first_state_id)))
+classes = {"amenities": Amenity, "cities": City,"places": Place,
+           "reviews": Review, "states": State, "users": User}
+counts ={}
+for key, obj in classes.items():
+    counts[key] = storage.count(obj)
+print(counts)
